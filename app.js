b6d53dee0e398be0513c03857ee1481b06b0da72
suraj171299 <surajpadalkar1999@gmail.com>
2023-09-21 12:49:54 +0530

    Delete Function

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
