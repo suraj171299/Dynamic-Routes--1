8055cf3928818958fa654ba3717e08c8139dc91c
suraj171299 <surajpadalkar1999@gmail.com>
2023-09-21 13:21:21 +0530

    Sequelize Integration

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 
 const app = express();
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,4 +22,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+    .sync()
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch((err) => console.log(err));
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-project", "root", "8879", {
+    dialect: "mysql",
     host: "localhost",
-    user: "root",
-    database: "node-project",
-    password: "8879",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
